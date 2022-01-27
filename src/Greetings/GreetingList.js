27ea1bd76822a8bac3510cf1498640a64943f0a0
TrevorRezac <trev.rezac@gmail.com>
2022-01-27 12:16:00 -0800

    created greeting css file

--- src/Greetings/GreetingList.js
+++ src/Greetings/GreetingList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Greeting from './Greeting.js';
-
+import './Greeting.css';
 
 export default function GreetingList({ greetings }) {
   return <div className='greeting-list'>
