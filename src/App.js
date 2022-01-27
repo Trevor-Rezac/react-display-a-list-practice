f6abf44a5d8e7ca97b6084d5e950fcc038ed1ab5
TrevorRezac <trev.rezac@gmail.com>
2022-01-27 14:17:18 -0800

    dog list displays correctly with minor styling

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import { greetings } from './Greetings/greetings';
 import GreetingList from './Greetings/GreetingList.js';
 import { friends } from './Friends/friends.js';
 import FriendList from './Friends/FriendList.js';
+import { dogs } from './Dogs/dogs.js';
+import DogList from './Dogs/DogList.js';
 
 
 function App() {
-  
+  console.log(dogs);
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
       <GreetingList greetings={greetings} />
       <FriendList friends={friends} />
+      <DogList dogs={dogs} />
     </div>
   );
 }
---
+++ src/Dogs/dogs.js
@@ -0,0 +1,29 @@
+export const dogs = [
+  {
+    name: 'Fido',
+    age: 5,
+    nemesis: {
+      name: 'Felix',
+      age: 5,
+      type: 'cat'
+    }
+  },
+  {
+    name: 'Spy',
+    age: 6,
+    nemesis: {
+      name: 'Spy',
+      age: 9,
+      type: 'lizard'
+    }
+  },
+  {
+    name: 'La-Z Boy',
+    age: 2,
+    nemesis: {
+      name: 'Cat',
+      age: 1,
+      type: 'vacuum'
+    }
+  },
+];
